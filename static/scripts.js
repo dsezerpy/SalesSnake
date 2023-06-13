80dcf49f9cc5d9aff9a38de149a0336102339eca
Nillays <aylinlillay@gmail.com>
2023-06-13 18:41:08 +0300

    doing css

--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,3 @@
-function openForm() {
-  document.getElementById("addStock").style.display = "flex";
-}
-
-function closeForm() {
-  document.getElementById("addStock").style.display = "none";
-}
-
-function openEditForm() {
-  document.getElementById("editStock").style.display = "flex";
-}
-
-function closeEditForm() {
-  document.getElementById("editStock").style.display = "none";
-}
 
 function setDate(target){
   target.valueAsDate=new Date();
